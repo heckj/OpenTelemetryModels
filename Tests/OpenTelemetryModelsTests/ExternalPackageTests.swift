364380221b385cee475bc5f0658d3969bcb98573
Joe Heck <heckj@mac.com>
2020-04-14 12:45:33 -0700

    adding some basic defensive tests for models

--- Tests/OpenTelemetryModelsTests/ExternalPackageTests.swift
+++ Tests/OpenTelemetryModelsTests/ExternalPackageTests.swift
@@ -9,10 +9,25 @@ import OpenTelemetryModels
 import XCTest
 
 final class PackagingTests: XCTestCase {
-    func testExposedClasses() {
+    func testExposedClassesExist() {
         // verifies the struct is visible externally - otherwise this won't compile
-        let _ = Opentelemetry_Proto_Trace_V1_Span()
+        // in proto 0.3.0 - trace is beta
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Span()
         let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Status()
         let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_TraceConfig()
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Span()
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Status()
+        
+        // in proto 0.3.0 - resource is beta
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Resource_V1_Resource()
+        
+        // in proto 0.3.0 - common is beta
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Common_V1_StringKeyValue()
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Common_V1_AttributeKeyValue()
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Common_V1_InstrumentationLibrary()
+
+        // in proto 0.3.0 - resource is alpha
+        let _ = OpenTelemetryModels.Opentelemetry_Proto_Metrics_V1_Metric()
+        
     }
 }
