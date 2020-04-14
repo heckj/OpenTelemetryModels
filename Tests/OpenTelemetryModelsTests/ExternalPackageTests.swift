d82af680013409969dad72533f1446085714f411
Joe Heck <heckj@mac.com>
2020-04-14 13:41:11 -0700

    expanding classes verified to exist with tests

--- Tests/OpenTelemetryModelsTests/ExternalPackageTests.swift
+++ Tests/OpenTelemetryModelsTests/ExternalPackageTests.swift
@@ -12,22 +12,26 @@ final class PackagingTests: XCTestCase {
     func testExposedClassesExist() {
         // verifies the struct is visible externally - otherwise this won't compile
         // in proto 0.3.0 - trace is beta
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Span()
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Status()
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_TraceConfig()
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Span()
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Trace_V1_Status()
+        let _ = Opentelemetry_Proto_Trace_V1_Span()
+        let _ = Opentelemetry_Proto_Trace_V1_Status()
+        let _ = Opentelemetry_Proto_Trace_V1_TraceConfig()
+        let _ = Opentelemetry_Proto_Trace_V1_Span()
+        let _ = Opentelemetry_Proto_Trace_V1_Status()
+        let _ = Opentelemetry_Proto_Trace_V1_ResourceSpans()
+        let _ = Opentelemetry_Proto_Trace_V1_ConstantSampler()
+        let _ = Opentelemetry_Proto_Trace_V1_ProbabilitySampler()
+        let _ = Opentelemetry_Proto_Trace_V1_RateLimitingSampler()
         
         // in proto 0.3.0 - resource is beta
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Resource_V1_Resource()
+        let _ = Opentelemetry_Proto_Resource_V1_Resource()
         
         // in proto 0.3.0 - common is beta
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Common_V1_StringKeyValue()
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Common_V1_AttributeKeyValue()
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Common_V1_InstrumentationLibrary()
+        let _ = Opentelemetry_Proto_Common_V1_StringKeyValue()
+        let _ = Opentelemetry_Proto_Common_V1_AttributeKeyValue()
+        let _ = Opentelemetry_Proto_Common_V1_InstrumentationLibrary()
 
         // in proto 0.3.0 - resource is alpha
-        let _ = OpenTelemetryModels.Opentelemetry_Proto_Metrics_V1_Metric()
+        let _ = Opentelemetry_Proto_Metrics_V1_Metric()
         
     }
 }
