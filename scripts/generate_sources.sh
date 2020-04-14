ed7c84db3438ed83ecc2f7308b1d3a6ad988bff2
Joe Heck <heckj@mac.com>
2020-04-13 20:20:02 -0700

    drop the path in the generated output

--- scripts/generate_sources.sh
+++ scripts/generate_sources.sh
@@ -3,6 +3,6 @@ FILES=$(find opentelemetry/proto/*/v1 -name "*.proto")
 
 for FILE in ${FILES}
 do
-    protoc --swift_out=../Sources/OpenTelemetryModels $FILE
+    protoc --swift_opt=FileNaming=DropPath --swift_out=../Sources/OpenTelemetryModels $FILE
 done
 popd
