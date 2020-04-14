fdbeedbe3f21091ffd86ea1656321231e8aa02c4
Joe Heck <heckj@mac.com>
2020-04-13 20:10:27 -0700

    CI quickie

--- Tests/OpenTelemetryModelsTests/ExternalPackageTests.swift
+++ Tests/OpenTelemetryModelsTests/ExternalPackageTests.swift
@@ -11,7 +11,7 @@ import XCTest
 final class PackagingTests: XCTestCase {
     func testManualTicks() {
         let x = OpenTelemetryModels.Example()
-        let trace = Opentelemetry_Proto_Trace_V1_Span()
+        //let trace = Opentelemetry_Proto_Trace_V1_Span()
         // verifies the struct is visible externally - else this won't compile
     }
 }
