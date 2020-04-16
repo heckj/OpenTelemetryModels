68324c0cce055d3aad328f3dfe5e63f518e9ac36
Joe Heck <heckj@mac.com>
2020-04-16 15:22:18 -0700

    clearing the deadwood

--- Tests/OpenTelemetryModelsTests/OpenTelemetryModelsTests.swift
+++ Tests/OpenTelemetryModelsTests/OpenTelemetryModelsTests.swift
@@ -6,7 +6,7 @@ final class OpenTelemetryModelsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(OpenTelemetryModels.Example().text, "Hello, World!")
+        XCTAssertEqual(true, true)
     }
 
     static var allTests = [
